243b28544c67d7d542d650d8cf1fe0c15c73bac0
thereisnotspoon <byronrpv_rckr@hotmail.com>
2018-02-06 16:20:44 -0500

    Crear estructura de datos de prueba

--- ClienteWSProsur/src/client/use/ClienteUso.java
+++ ClienteWSProsur/src/client/use/ClienteUso.java
@@ -16,11 +16,11 @@ public class ClienteUso {
 		ProsurIPRecordWSService service = new ProsurIPRecordWSService();
 		ProsurIPRecordWS port = service.getProsurIPRecordWSPort();
 		
-		IpRecord ipRecord = new IpRecord();
 		IpRecordTypeEnum ipType = IpRecordTypeEnum.DISTINCTIVE_SIGN;
 		
 		ObjectFactory objectFactory = new ObjectFactory();
 		
+		//manejo de fechas
 		Date nationalDate = new Date();
 		nationalDate.getTime();
 		GregorianCalendar c = new GregorianCalendar();
@@ -33,17 +33,37 @@ public class ClienteUso {
 			e.printStackTrace();
 		}
 
+				
+		DistinctiveSign distinctiveSign = new DistinctiveSign();
+		NiceClass nice1 = new NiceClass();
+		nice1.setActivity("dummy activity for nice1");
+		nice1.setNiceClass(1);
 		
-		ipRecord.setIpRecordId(objectFactory.createIpRecordIpRecordId((long) 6969));
-		ipRecord.setOnapiId(objectFactory.createIpRecordOnapiId("EC"));
-		ipRecord.setApplicationId(objectFactory.createIpRecordApplicationId("1706136665486666"));
-		ipRecord.setRecordType(objectFactory.createIpRecordRecordType(ipType));
-		ipRecord.setNationalPresentationDate(objectFactory.createIpRecordNationalPresentationDate(date2));
-		ipRecord.setNationalPublishingDate(objectFactory.createIpRecordNationalPublishingDate(date2));
-		ipRecord.setTitle(objectFactory.createIpRecordTitle("titulo de relleno"));
-		ipRecord.getApplicantName().add("Jose Maria Chipugsi Catota, Bill Gates Quishpe");
-		ipRecord.setStatusId(objectFactory.createIpRecordStatusId("EN_TRAMITE"));
-		ipRecord.setIpRecordFilesService(objectFactory.createIpRecordIpRecordFilesService("aqui deberia ir la url del ws del IEPI"));
+		distinctiveSign.setIpRecordId(objectFactory.createIpRecordIpRecordId((long) 6969));
+		distinctiveSign.setOnapiId(objectFactory.createIpRecordOnapiId("EC"));
+		distinctiveSign.setApplicationId(objectFactory.createIpRecordApplicationId("1706136665486666"));
+		distinctiveSign.setRecordType(objectFactory.createIpRecordRecordType(ipType));
+		distinctiveSign.setNationalPresentationDate(objectFactory.createIpRecordNationalPresentationDate(date2));
+		distinctiveSign.setNationalPublishingDate(objectFactory.createIpRecordNationalPublishingDate(date2));
+		distinctiveSign.setTitle(objectFactory.createIpRecordTitle("titulo de relleno"));
+		distinctiveSign.getApplicantName().add("Jose Maria Chipugsi Catota, Bill Gates Quishpe");
+		distinctiveSign.setStatusId(objectFactory.createIpRecordStatusId("EN_TRAMITE"));
+		distinctiveSign.setIpRecordFilesService(objectFactory.createIpRecordIpRecordFilesService("aqui deberia ir la url del ws del IEPI"));
+		distinctiveSign.setDistinctiveSignType(objectFactory.createDistinctiveSignDistinctiveSignType("MARCA"));
+		distinctiveSign.setExpiration(objectFactory.createDistinctiveSignExpiration(date2));
+		distinctiveSign.setLogoDescription(objectFactory.createDistinctiveSignLogoDescription("lorem ipsum consequat"));
+		distinctiveSign.getNiceClasses().add(nice1);
+		distinctiveSign.setRegistrationDate(objectFactory.createDistinctiveSignRegistrationDate(date2));
 		
+		String user = "ecu_user";
+		System.out.println("hola");
+
+		try {
+			port.createRecord(distinctiveSign, user);
+		} catch (CatalogFault_Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
